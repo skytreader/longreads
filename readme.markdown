cf3f0373c495ce61efe900baa8ac2087bc52a2f3
Chad Estioco <chadestioco@gmail.com>
2021-12-01 05:22:21 +0800

    Proof-of-concept implementation, some API changes.

--- readme.markdown
+++ readme.markdown
@@ -1,8 +1,9 @@
 # longreads
 
 A minimalist JS library for bookmarking sections in longform web textual content.
-This helps your website remember the last section of text a visitor has been
-reading, all without the necessity of a backend store.
+This helps your website remember the last section a visitor has been reading,
+all without the necessity of a backend store. This makes the assumption that
+your content is properly structured with `<section>` tags.
 
 The emphasis on minimalism has two objectives:
 
@@ -15,8 +16,9 @@ The library exposes three methods:
 
 - `setBookmark(article: string, section: string): void`
 - `getBookmark(article: string): string`
-- `isCurrentlyViewed(section: string): boolean` is a helper function to
-determine whether your visitor has reached the given section.
+- `getCurrentlyViewed(): string[]` is a helper function to determine which
+sections are currently in the viewport. The result is sorted by their top-to-
+bottom layout order.
 
 `article` is a unique string that identifies the piece of text a visitor could
 read.
--- src/index.ts
+++ src/index.ts
@@ -25,4 +25,36 @@ class DocumentCookieManager {
 const dcm = new DocumentCookieManager();
 
 export function setBookmark(article: string, section: string): void {
+    dcm.add(article, section);
+}
+
+export function getBookmark(article: string): string {
+    return dcm.getVal(article);
+}
+
+function isInViewPort(el: HTMLElement): boolean {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+export function getCurrentlyViewed(): string[] {
+    return [].slice.call(document.getElementsByTagName("section"))
+        .filter(isInViewPort)
+        // TODO Is a sort necessary? Check docs!
+        .sort((el1: HTMLElement, el2: HTMLElement) => {
+            const rec1 = el1.getBoundingClientRect();
+            const rec2 = el2.getBoundingClientRect();
+            if (rec1.top < rec2.top) {
+                return -1;
+            } else if (rec1.top > rec2.top) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
 }
